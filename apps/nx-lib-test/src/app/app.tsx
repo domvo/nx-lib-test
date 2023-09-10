f4ca70be1794ccac5235c1d83672ae089cb1253d
domvo <dominik.voss@fides.technology>
2023-09-10 14:34:33 +0200

    test setup

--- apps/nx-lib-test/src/app/app.tsx
+++ apps/nx-lib-test/src/app/app.tsx
@@ -1,12 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
+import { SomeFeLib } from '@nx-lib-test/some-fe-lib/SomeComp';
+
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
       <NxWelcome title="nx-lib-test" />
+      <SomeFeLib />
     </div>
   );
 }
---
+++ libs/some-fe-lib/src/lib/SomeComp/index.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface SomeFeLibProps {}
+
+export function SomeFeLib(props: SomeFeLibProps) {
+  return (
+    <div>
+      <h1>Welcome to SomeFeLib!</h1>
+    </div>
+  );
+}
+
+export default SomeFeLib;
